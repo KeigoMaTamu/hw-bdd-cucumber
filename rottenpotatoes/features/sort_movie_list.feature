be0e2f75e953d3eb33c6569c7042824065d50df1
EC2 Default User <ec2-user@ip-172-31-2-230.us-east-2.compute.internal>
2021-10-05 02:49:38 +0000

    Part 1 and some part 2

--- rottenpotatoes/features/sort_movie_list.feature
+++ rottenpotatoes/features/sort_movie_list.feature
@@ -25,7 +25,10 @@ Background: movies have been added to database
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
   # your steps here
-
+  Then I should see "Aladdin" before "Amelie"
+  And I should see "Chocolat" before "The Help"
 Scenario: sort movies in increasing order of release date
   When I follow "Release Date"
   # your steps here
+  Then I should see "Alladin" before "The Help"
+  And I should see "Raiders of the Lost Ark" before "The Incredibles"
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,11 +1,17 @@
 # Add a declarative step here for populating the DB with movies.
 
+num_rows=0
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    num_row+=1
   end
   fail "Unimplemented"
+end
+
+When /^I click '(.*)'/ do |button|
+  click_button button
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
