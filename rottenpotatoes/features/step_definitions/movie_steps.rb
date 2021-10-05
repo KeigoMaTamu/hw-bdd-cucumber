0401596ddf4ac9bf7f1134f420e17bfd1b26eb03
EC2 Default User <ec2-user@ip-172-31-2-230.us-east-2.compute.internal>
2021-10-05 03:14:53 +0000

    some part 2 and part 3 test

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,18 +1,14 @@
 # Add a declarative step here for populating the DB with movies.
 
-num_rows=0
+
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    num_row+=1
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
 
-When /^I click '(.*)'/ do |button|
-  click_button button
-end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
   expect(Movie.count).to eq n_seeds.to_i
@@ -22,9 +18,11 @@ end
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  position_e1 = page.body.index(e1)  
+  position_e2 = page.body.index(e2)
+  if position_e1.should_not > position_e2
+    fail "Wrong order"
+  end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -32,13 +30,20 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(",").each do | rating |
+    rating = "ratings_" + rating
+    if uncheck
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
 end
+
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
+  rows = page.all('table#movies tbody tr').length
+  rows.should eq Movie.count
   fail "Unimplemented"
 end
